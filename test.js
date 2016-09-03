46808310e2785dc40ec9cb7aab28b2149d16a852
Christopher Bennage <christopher@bennage.com>
2016-09-02 19:29:25 -0700

    code reorg

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const processFiles = require('./lib/processFiles');
 const listFiles = require('./lib/listFiles');
-const renderForConsole = require('./lib/renderForConsole');
+const renderForConsole = require('./lib/render');
 
 listFiles('./test', '**/*.md')
     .then(processFiles)
