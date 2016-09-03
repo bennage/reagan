d7c229eda63f51171e69a605f77b174afbeb827b
Christopher Bennage <christopher@bennage.com>
2016-09-02 20:06:25 -0700

    enabling a strict option

--- test.js
+++ test.js
@@ -2,7 +2,12 @@ const processFiles = require('./lib/processFiles');
 const listFiles = require('./lib/listFiles');
 const renderForConsole = require('./lib/render');
 
-listFiles('./test', '**/*.md')
+var ctx = {
+    cwd: './test',
+    pattern: '**/*.md'
+};
+
+listFiles(ctx)
     .then(processFiles)
     .then(renderForConsole)
     .catch(err => console.dir);
