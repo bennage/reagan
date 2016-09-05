6286f00ef232255a3fc6e122a29f0217fb368511
Christopher Bennage <christopher@bennage.com>
2016-09-04 17:35:19 -0700

    going crazy

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
-const processFiles = require('./lib/processFiles');
-const listFiles = require('./lib/listFiles');
-const update = require('./lib/update');
-const render = require('./render');
+const chalk = require('chalk');
+const render = require('./lib/render');
+const app = require('./lib/app');
+const Context = require('./lib/context');
 
-var ctx = {
+var ctx = new Context({
     cwd: './test',
     pattern: '**/*.md',
     attemptUpdate: true
-};
+});
 
-listFiles(ctx)
-    .then(processFiles)
-    .then(update)
-    .then(render)
-    .catch(err => console.dir);
+render(ctx);
+
+app(ctx)
+    .then(_ => { process.exit(); })
+    .catch(err => {
+        console.dir(err);
+        process.exit(1);
+    });
